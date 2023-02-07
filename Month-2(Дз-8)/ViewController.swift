e292e284dcc8cab8fb4e18d8f6e94a74a7cc1204
Nurjamal <n.mirbaizaeva@gmail.com>
2023-02-07 17:52:44 +0600

    UI

--- Month-2(Дз-8)/ViewController.swift
+++ Month-2(Дз-8)/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var numberOne: UITextField!
+    
+    @IBOutlet weak var numberTwo: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func add(_ sender: Any) {
+        let number1 = numberOne.text!
+        let number2 = numberTwo.text!
+        
+        let resultInt = Int(number1)! + Int(number2)!
+        result.text = "\(resultInt)"
+    }
+    
+    @IBAction func subtraction(_ sender: Any) {
+        let number1 = numberOne.text!
+        let number2 = numberTwo.text!
+        
+        let resultInt = Int(number1)! - Int(number2)!
+        result.text = "\(resultInt)"
+    }
+
+    @IBAction func multiplication(_ sender: Any) {
+        let number1 = numberOne.text!
+        let number2 = numberTwo.text!
+        
+        let resultInt = Int(number1)! * Int(number2)!
+        result.text = "\(resultInt)"
+    }
+
+    @IBAction func division(_ sender: Any) {
+        let number1 = numberOne.text!
+        let number2 = numberTwo.text!
+        
+        let resultInt = Int(number1)! / Int(number2)!
+        result.text = "\(resultInt)"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
+
 
